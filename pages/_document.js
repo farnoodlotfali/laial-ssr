3d4dfaa93a1853159c43d1dbe0cbc8e117c0b656
farnood lotfali <farnoodlotfali@gmail.com>
2021-10-11 23:23:35 +0330

    Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -15,24 +15,25 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           {/* <!-- Google Tag Manager --> */}
-          {/* <script>
-            {(function (w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-              var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != "dataLayer" ? "&l=" + l : "";
-              j.async = true;
-              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-              f.parentNode.insertBefore(j, f);
-            })(
-              process.browser && window,
-              process.browser && document,
-              "script",
-              "dataLayer",
-              "GTM-5PRF3GZ"
-            )}
-          </script> */}
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                `(function(w,l){` +
+                ` w[l] = w[l] || [];
+                w[l].push({
+                  "gtm.start": new Date().getTime(),
+                  event: "gtm.js",
+                });
+                var f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s),
+                  dl = l != "dataLayer" ? "&l=" + l : "";
+                j.async = true;
+                j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+                f.parentNode.insertBefore(j, f);` +
+                `})(window, document, "script", "dataLayer", "GTM-5PRF3GZ");`,
+            }}
+          />
           {/* <!-- End Google Tag Manager --> */}
           <link rel="icon" href="/favicon_safine.png" />
         </Head>
